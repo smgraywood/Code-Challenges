fa6d453bce1084eedfd49a14205ef22f9929a5f7
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2022-08-09 10:55:10 -0700

    Update Code Challenge 08.09.22.js

--- Code Challenge 08.09.22.js
+++ Code Challenge 08.09.22.js
@@ -6,3 +6,26 @@
 
 // has22([2, 1, 2]) → false
 
+function has22(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 2 && numbers[i+1] === 2) {
+      return true
+    } 
+  }
+  return false
+}
+
+console.log(has22([1, 2, 2])) //→ true
+
+console.log(has22([1, 2, 1, 2])) //→ false
+
+console.log(has22([2, 1, 2])) //→ false
+
+
+//if array[i] === 2 and array[i]++ ==== 2 then return True
+
+
+//for loop
+//create an empty array to put 2s into
+//return bool
+//if/else
