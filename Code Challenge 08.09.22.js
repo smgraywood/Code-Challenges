f95de4f0756b847dae41e389e48fa70f68ccd2b5
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2022-08-09 10:19:39 -0700

    Create Code Challenge 08.09.22.js

---
+++ Code Challenge 08.09.22.js
@@ -0,0 +1,8 @@
+// Given an array of ints, return True if the array contains a 2 next to a 2 somewhere.
+
+// has22([1, 2, 2]) → true
+
+// has22([1, 2, 1, 2]) → false
+
+// has22([2, 1, 2]) → false
+
