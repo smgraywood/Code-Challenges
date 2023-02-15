491de48e337606bf7ee99af1e3d147f45b6b95b7
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-02-15 13:36:53 -0800

    Pangram

---
+++ Code Challenge 02.15.23.js
@@ -0,0 +1,110 @@
+// A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. Ignore case. Return either pangram or not pangram as appropriate.
+
+// Example
+
+// The string contains all letters in the English alphabet, so return pangram.
+
+// Function Description
+
+// Complete the function pangrams in the editor below. It should return the string pangram if the input string is a pangram. Otherwise, it should return not pangram.
+
+// pangrams has the following parameter(s):
+
+// string s: a string to test
+// Returns
+
+// string: either pangram or not pangram
+// Input Format
+
+// A single line with string .
+
+// Constraints
+
+
+// Each character of , 
+
+// Sample Input
+
+// Sample Input 0
+
+// We promptly judged antique ivory buckles for the next prize
+
+// Sample Output 0
+
+// pangram
+
+// Sample Explanation 0
+
+// All of the letters of the alphabet are present in the string.
+
+// Sample Input 1
+
+// We promptly judged antique ivory buckles for the prize
+
+// Sample Output 1
+
+// not pangram
+
+// Sample Explanation 0
+
+// The string lacks an x.
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+function pangrams(s) {
+    let upperCase = s.toUpperCase()
+    let counter = 0
+    for(let i = 0; i<upperCase.length; i++){
+        if (upperCase.includes(alphabet[i])=== true){
+            counter++
+        }    
+    }
+    if (counter === 26){
+        return "pangram"
+    }
+    return 'not pangram'
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = pangrams(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
