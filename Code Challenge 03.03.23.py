aad8f1531ab9a46baa2cba119a387855161adb9e
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-03-03 12:59:07 -0800

    Rotate Array

---
+++ Code Challenge 03.03.23.py
@@ -0,0 +1,9 @@
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
