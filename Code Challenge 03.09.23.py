59d1f0d3966e658a223fae3beed2a9e9e465bc40
Sarah Graywood <81881720+smgraywood@users.noreply.github.com>
2023-03-09 13:33:36 -0800

    Remove Nth Node From End of List

---
+++ Code Challenge 03.09.23.py
@@ -0,0 +1,11 @@
+# Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        def remove(head):
+            if not head:
+                return 0, head
+            i, head.next = remove(head.next)
+            return i+1, (head, head.next)[i+1 == n]
+        return remove(head)[1]
